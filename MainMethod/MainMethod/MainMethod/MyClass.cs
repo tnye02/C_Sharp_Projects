75cc9646e2d71b3792f97934a533bf74643c8df8
tnye02 <tnye02@gmail.com>
2021-12-23 21:14:28 -0800

    MainMethod

---
+++ MainMethod/MainMethod/MainMethod/MyClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MyClass
+    {
+        public static int DoThings(int myInteger)
+        {
+            int answer = (myInteger * 2) + myInteger;
+            return answer;
+        }
+
+        public static double DoThings(double myDouble)
+        {
+            double answer = myDouble * 100;
+            return answer;
+        }
+
+        public static string DoThings(string myString)
+        {
+            int myInt;
+            try
+            {
+                if (int.TryParse(myString, out myInt))
+                {
+                    int answer = (DoThings(myInt) + 42) * 42;
+                    string success = "\n" + myInt + " plus 42 times 42 equals " + answer;
+                    return success;
+                }
+                else
+                {
+                    string fail = "\nSorry I could not convert " + myString + " to an integer.";
+                    return fail;
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                return error;
+            }
+
+        }
+    }
+}
---
+++ MainMethod/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer: ");
+            int theChoice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n" + theChoice + " times 2 plus " + theChoice + " equals " + MyClass.DoThings(theChoice));
+
+            Console.WriteLine("\nPlease enter a decimal: ");
+            double theChoice2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n" + theChoice2 + " times 100 equals " + MyClass.DoThings(theChoice2));
+
+            Console.WriteLine("\nPlease enter an integer (you can try a string as well):");
+            string theChoice3 = Console.ReadLine();
+
+            Console.WriteLine(MyClass.DoThings(theChoice3));
+
+            Console.WriteLine("\nPlease press ENTER to exit.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
