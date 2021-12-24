2467580498703d62a3145bef948b366117bab46a
tnye02 <tnye02@gmail.com>
2021-12-23 16:56:39 -0800

    Calling Methods

---
+++ CallingMethods/CallingMethods/CallingMethods/DoMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class DoMath
+    {
+        public static double Squared(double number)
+        {
+            double choiceSquared = Math.Pow(number, 2);                                                 // Finds the square of the input and returns the result to the Main method
+            return choiceSquared;
+        }
+
+        public static double Third(double number)                                                       // Finds the third power of the input and returns the result to the Main method
+        {
+            double choiceThird = Math.Pow(number, 3);
+            return choiceThird;
+        }
+
+        public static double Final(double number)                                                       // Does the custom calculation required and returns the result to the Main method
+        {
+            double choiceFinal = (number * 2) + DoMath.Squared(number) + DoMath.Third(number);
+            return choiceFinal;
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");                                                                                                         // Asks user for an integer
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(userChoice + " squared is: " + DoMath.Squared(userChoice));                                                                       // Displays the answer using the Squared method in the DoMath class
+
+            Console.WriteLine(userChoice + " to the third power is: " + DoMath.Third(userChoice));                                                              // Displays the answer using the Third method in the DoMath class
+
+            double finalCalc = DoMath.Final(userChoice);                                                                                                        // Calls the Final method in the DoMath class and sets the result to a variable
+
+            Console.WriteLine(userChoice + " times 2 plus " + userChoice + " squared plus " + userChoice + " to the third power is: " + finalCalc);             // Displays the final answer
+
+            Console.ReadLine();
+        }
+    }
+}
