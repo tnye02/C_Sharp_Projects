2c9b8fbdcc36c49e7a39b2e45670c90e81409c67
tnye02 <tnye02@gmail.com>
2021-12-24 13:31:57 -0800

    MethodStep 280

--- MethodStep280/MethodStep280/MethodStep280/MyMethod.cs
+++ MethodStep280/MethodStep280/MethodStep280/MyMethod.cs
@@ -8,5 +8,10 @@ namespace MethodStep280
 {
     class MyMethod
     {
+        public static int DoThings(int int1, int int2 = 0)                  // Method that adds two integers together with an optional second integer set to zero
+        {
+            int answer = int1 + int2;
+            return answer;
+        }
     }
 }
--- MethodStep280/MethodStep280/MethodStep280/Program.cs
+++ MethodStep280/MethodStep280/MethodStep280/Program.cs
@@ -10,7 +10,38 @@ namespace MethodStep280
     {
         static void Main(string[] args)
         {
+            int myInt1;
+            int myInt2;
+            Console.WriteLine("Please enter one or two integers.  \nThe second one is optional and defaults to zero.");         // Instructions to user
+            Console.Write("Integer 1: ");
+            myInt1 = Convert.ToInt32(Console.ReadLine());                                                                       // User inputs integer 1
 
+            Console.Write("Would you like to skip the second integer? y/n: ");                                                  // Ask the user if they want to enter the second integer
+            string getChoice = Console.ReadLine();
+
+            string lowerChoice = getChoice.ToLower();
+
+            while (lowerChoice != "y" && lowerChoice != "n")                                                                    // While loop that only allows y or n    
+            {
+                Console.Write("Please enter y or n: ");
+                getChoice = Console.ReadLine();
+                lowerChoice = getChoice.ToLower();
+            }
+
+            if (lowerChoice == "y")                                                                                             // If statement branches to either possibility
+            {
+                MyMethod.DoThings(myInt1);
+                Console.WriteLine("\nAdding your number to zero yields: " + MyMethod.DoThings(myInt1));
+            }
+            else
+            {
+                Console.Write("Integer 2: ");
+                myInt2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nAdding your two numbers together yields: " + MyMethod.DoThings(myInt1, myInt2));
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
         }
     }
 }
