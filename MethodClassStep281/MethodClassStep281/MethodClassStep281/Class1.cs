2a0eb61033445dfe79c4c6cf19e3b829a301e016
tnye02 <tnye02@gmail.com>
2021-12-24 18:28:05 -0800

    MethodClassStep281

---
+++ MethodClassStep281/MethodClassStep281/MethodClassStep281/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassStep281
+{
+    class Class1
+    {
+        public void myMethod (int int1, int int2)                       // Void method does not return any data
+        {
+            int answer = int1 * 764;                                    // Unused math operation on the first integer
+            Console.WriteLine("The second integer is: " + int2);        // Displays the second integer
+        }
+
+    }
+}
---
+++ MethodClassStep281/MethodClassStep281/MethodClassStep281/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassStep281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();                       // Instantiates the class Class1
+
+            class1.myMethod(2, 4);                              // Calls the method with just two numbers
+            class1.myMethod(int1: 3, int2: 5);                  // Calls the method specifiying the parameters by name
+
+            Console.ReadLine();
+        }
+    }
+}
