d230462e2d644ce52be165aba705d542c961dc8e
tnye02 <tnye02@gmail.com>
2022-01-01 14:45:44 -0800

    AbstractClass_Step302

---
+++ AbstractClass_Step302/AbstractClass_Step302/AbstractClass_Step302/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Step302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();             // Instantiate new employee object
+            employee.firstName = "Bob";                     // Set first name to Bob
+            employee.lastName = "Smith";                    // Set last name to Smith
+
+            employee.SayName();                             // Call SayName method on the object
+            Console.ReadLine();
+
+        }
+    }
+}
