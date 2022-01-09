1c4fca566b4fe87f9e04166e38828a2c245ec83c
tnye02 <tnye02@gmail.com>
2022-01-08 20:55:47 -0800

    ChainedConstructors_Step383

---
+++ Constructors_Step383/Constructors_Step383/FullName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Step383
+{
+    public class FullName
+    {
+        //------------ Chained constructor method -------------
+        public FullName(string fName, string lName) : this(fName, "<<NO MIDDLE NAME PROVIDED>>", lName)
+        {
+
+        }
+
+        //------------ Original constructor method -------------
+        public FullName(string fName, string midName, string lName)
+        {
+            string first_name = fName;
+            string last_name = lName;
+            string middle_name = midName;
+            string full_name = fName + " " + midName + " " + lName;
+
+            Console.WriteLine("Full name: " + full_name);
+        }
+    }
+}
---
+++ Constructors_Step383/Constructors_Step383/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Step383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------ Declare a constante for the title of the program -------------
+            const string title = "--------------------------------\nThis is the Title of the Program\n--------------------------------";
+            //------------ Display the title -------------
+            Console.Write(title);
+            //------------ Ask user for various names -------------
+
+            Console.Write("\nPlease enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("\nPlease enter your middle name or press enter: ");
+            string middleName = Console.ReadLine();
+
+            Console.Write("\nPlease enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            //------------ Call constructor with two parameters supplied and with three -------------
+            FullName name_object = new FullName(firstName, lastName);
+            FullName name_object2 = new FullName(firstName, middleName, lastName);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
