6fe7e71397fbe52996f19d1f9cc6875a4916f061
tnye02 <tnye02@gmail.com>
2022-01-18 20:11:41 -0800

    ServerTimeProject

---
+++ ServerTime/ServerTime/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerTime.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateTimeOffset myTime = DateTimeOffset.Now;
+
+            ViewData["TimeStamp"] = myTime.ToString("%h:mm tt");
+        }
+    }
+}
